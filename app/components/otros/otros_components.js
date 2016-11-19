28c3eec8e6545db5c9bd3fecbfb08def11061b94
Brenda Gonzalez <bremin11.20.93@gmail.com>
2016-11-19 13:37:17 -0600

    subelo pussu

--- app/components/otros/otros_components.js
+++ app/components/otros/otros_components.js
@@ -6,28 +6,11 @@
 	}
 	
 	function otrosController(){
-		var otros = this;
-		otros.arts = arts;
+		 $('.carousel').carousel();
+
 	}
 
-	var arts = [
-		{
-            nombre:'Accesorio1',
-            descripcion:'Hola yo soy panel solar 1',
-            imagen:'http://www.fancyicons.com/free-icons/232/industry/png/256/solar_panel_256.png'
-        },
-        {
-            nombre:'Accesorio2',
-            descripcion:'Hola yo soy panel solar 1',
-            imagen:'http://www.fancyicons.com/free-icons/232/industry/png/256/solar_panel_256.png'
-        },
-        {
-            nombre:'Accesorio3',
-            descripcion:'Hola yo soy panel solar 1',
-            imagen:'http://www.fancyicons.com/free-icons/232/industry/png/256/solar_panel_256.png'
-        },
-	]
-
+	
 
 
 	angular
